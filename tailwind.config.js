d5504d78bf782964a7dba277778a4e6ca6af1455
Paviyarasan <paviyarasantech@gmail.com>
2024-06-13 13:41:11 +0530

    commiting initial project set up

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primaryColor:"#22281E",
+          secondaryColor:"#14452F",
+          textColor:"#394630",
+          accentColor:"#14452F",
+          activeColor:"#7CFF77",
+          lightBlueColor:"#F2F8F1",
+          whiteColor:"#FFFFFF",
+          grayColor:"#E7E7E7"
+        }
+      }
+    }
+  }
